3278e0880a1c11cf0e7c7b051d855d6696503036
LLYS-Phys <lachyordanov@gmail.com>
2022-06-05 21:08:16 +0300

    add routing needed on header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website-cv-angular';
+  openedIntroduction = false
+  openedWorkExperience = false
+  openedEducation = false
+  openedSkills = false
+  openedAchievements = false
+  openedMoreAboutMe = false
+
+  clickIntroduction() {
+    this.openedIntroduction = true
+    this.openedWorkExperience = false
+    this.openedEducation = false
+    this.openedSkills = false
+    this.openedAchievements = false
+    this.openedMoreAboutMe = false
+  }
+
+  clickWorkExperience() {
+    this.openedIntroduction = false
+    this.openedWorkExperience = true
+    this.openedEducation = false
+    this.openedSkills = false
+    this.openedAchievements = false
+    this.openedMoreAboutMe = false
+  }
+
+  clickEducation() {
+    this.openedIntroduction = false
+    this.openedWorkExperience = false
+    this.openedEducation = true
+    this.openedSkills = false
+    this.openedAchievements = false
+    this.openedMoreAboutMe = false
+  }
+
+  clickSkills() {
+    this.openedIntroduction = false
+    this.openedWorkExperience = false
+    this.openedEducation = false
+    this.openedSkills = true
+    this.openedAchievements = false
+    this.openedMoreAboutMe = false
+  }
+
+  clickAchievements() {
+    this.openedIntroduction = false
+    this.openedWorkExperience = false
+    this.openedEducation = false
+    this.openedSkills = false
+    this.openedAchievements = true
+    this.openedMoreAboutMe = false
+  }
+
+  clickMoreAboutMe() {
+    this.openedIntroduction = false
+    this.openedWorkExperience = false
+    this.openedEducation = false
+    this.openedSkills = false
+    this.openedAchievements = false
+    this.openedMoreAboutMe = true
+  }
+
 }
