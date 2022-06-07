584ec832bff2175c1a0cf63f3deb68512f229be9
LLYS-Phys <lachyordanov@gmail.com>
2022-06-07 15:44:53 +0300

    WIP More about me page

--- src/app/more-about-me/more-about-me.component.ts
+++ src/app/more-about-me/more-about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-more-about-me',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class MoreAboutMeComponent implements OnInit {
 
   constructor() { }
+
+  @Input() titleMore = ''
 
   ngOnInit(): void {
   }
